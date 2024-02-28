eacab83f06563b1e5207403c1b209299c772ecd4
Nelson Martins <101191585+nels0Martins@users.noreply.github.com>
2024-02-28 19:39:51 -0300

    Add files via upload

---
+++ meu-site/scripts/script.js
@@ -0,0 +1,5 @@
+alert('Muito obrigado por acessar o meu portfólio! Essa é a primeira versão dele, estou sempre buscando melhorar. Aproveite, estou aceitando propostas e feedbacks!')
+
+function chamada() {
+    alert('Me indica no teu traampo, mano')
+}
